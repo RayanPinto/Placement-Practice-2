3ce75a4e94278e22690e6431026f214642a8dc3d
Rayan Pinto <rayanpinto9482@gmail.com>
2025-12-09 21:38:50 +0530

    learnedSomethingNewToday

---
+++ freq1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+struct node{
+    int data;
+    struct node* next;
+};
+int main(){
+struct node *head;
+struct node *second;
+struct node * third;
+head=(struct node*)malloc(sizeof(struct node));
+second=(struct node*)malloc(sizeof(struct node))
+return 0;
+
+}
