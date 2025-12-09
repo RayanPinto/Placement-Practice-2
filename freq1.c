823cae85a4e925287e6c13846b895899bb364f2b
Rayan Pinto <rayanpinto9482@gmail.com>
2025-12-09 21:43:03 +0530

    learnedSomethingNewToday

--- freq1.c
+++ freq1.c
@@ -3,12 +3,31 @@ struct node{
     int data;
     struct node* next;
 };
+void printhead(struct node* n){
+    while(n->next!=NULL){
+        printf("%d\t",n->data);
+        n=n->next;
+
+    }
+}
 int main(){
 struct node *head;
 struct node *second;
 struct node * third;
 head=(struct node*)malloc(sizeof(struct node));
-second=(struct node*)malloc(sizeof(struct node))
+second=(struct node*)malloc(sizeof(struct node));
+third=(struct node*)malloc(sizeof(struct node));
+head->data=10;
+head->next=second;
+second->data=20;
+second->next=third;
+third->data=20;
+third->next=NULL;
+printhead(head);
+free(head);
+free(second);
+free(third);
+
 return 0;
 
 }
